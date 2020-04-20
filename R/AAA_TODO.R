70ebeacb550a375ed08e86266814c9cb4c7c0393
James Hucklesby <jhuc964@aucklanduni.ac.nz>
2020-04-20 15:53:58 +1200

    ToDo updates

--- R/AAA_TODO.R
+++ R/AAA_TODO.R
@@ -1,8 +1,16 @@
 #' TODO
 #'
-#  Go through and name all the inappropriate ECISR functions vascular
-#' 
-#' Clean up ECIS_plot, and potentially subset some of it out into subserviant functions
+#' Fix well isolation plot
+#' Fix the continuous plot with multiple experiments and single well replication
+#' Fix error = 5
+#' Fix continuous strip_identical
+#' Trace down why cells.x and cells.y is cropping up. Suspect it's in implode???
+#'Fix ordering in ecis_anova
+#'
+#'Re-organise what's in what file
+#'
+#'
+#'update data conditioning in ecis_plot_continuous
 #' 
 #' Detection algorythm for wether a baked in or ecisr specific data table is used with potential code that allows for datasets to be revived as needed
 #' 
@@ -13,21 +21,23 @@
 #' Think about how ecis_subset_continous deals with veichles. Finish implimenting this.
 #' 
 #' ecis_implode but only non-0 things are written out + some kind of graphical representation of this
+#' Do this by making lots of rows and then imploding them
 #' 
 #' Make ECIS plot frequency and ecis-plot-model dependent on what is input
+#' Rename to ecis_plot_frequency_array and ecis_plot_model_array
 #' 
 #' Delete IsVehicleControl from graphics generation code
 #' 
 #' Line up data that I own and can include with the dataset
 #' 
-#' Move labeling, logging of axes and addition of points to ecis_polish_plot
+#' Move labeling, add addition of points to ecis_polish_plot
 #' 
-#' Optimise the speed of ecis_import_cellzscope, or remove it from CRAN checks
+#' Optimise the speed of ecis_import_cellzscope, and/or remove it from CRAN checks
 #' 
 #' Future R versions
 #' * Import CellZScope direct from file rather than through an intermediary
 #' * Resurect ecis_animate
 #' * Allow excell to be used for lookup files
 #' 
-
-
+#' 
+#' 
